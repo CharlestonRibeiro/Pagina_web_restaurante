40c69fa44eb0c86306c03ab8fa8502450a882a45
charleston <charles1235223@gmail.com>
2021-09-30 14:27:50 -0400

    add models e admin

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Cardapio
+
+@admin.register(Cardapio)
+class CardapioAdmin(admin.ModelAdmin):
+    list_display = ('nome','ingrediente','preco', 'ativo', 'modificado')
--- home/models.py
+++ home/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+from stdimage.models import StdImageField
+class Base(models.Model):
+    criados = models.DateField('Criação', auto_now_add=True)
+    modificado = models.DateField('Atualização', auto_now=True)
+    ativo = models.BooleanField('Ativo?', default=True)
+
+    class Meta:
+        abstract = True
+
+class Cardapio(Base):
+    nome = models.CharField('Nome', max_length=100)
+    ingrediente = models.CharField('Ingrediente', max_length=500)
+    preco =models.DecimalField('Preço', max_digits=5, decimal_places=2)
+
+    class Meta:
+        verbose_name = 'Cardapio'
+        verbose_name_plural = 'Cardipios'
+
+    def __str__(self):
+        return self.nome
+
